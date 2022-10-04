77e3bbb1561f887a1f8227b85fcc91dc51e0377c
GitStudent2022 <torelgame@mail.ru>
2022-10-04 14:45:45 +0300

    Добавьте файлы проекта.

---
+++ DynamicArrayBeginProject/DynamicArrayBeginProject.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class DArray
+{
+    int* items;
+    int size;
+public:
+    DArray() : size{ 0 }, items{ nullptr } {}
+    DArray(int size) : size{ size }
+    {
+        items = new int[size]{};
+    }
+    ~DArray()
+    {
+        if(items)
+            delete[] items;
+    }
+};
+
+void Func()
+{
+    DArray array(10);
+}
+
+int main()
+{
+    Func();
+
+    return 0;
+}
